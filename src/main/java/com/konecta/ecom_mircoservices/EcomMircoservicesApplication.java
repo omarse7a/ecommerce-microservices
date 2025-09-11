112deb75f76e8da3f13a6f01699eaf8e2066656f
omarse7a <omarsamehcs@gmail.com>
2025-09-11 09:11:39 +0300

    chore: initial commit

---
+++ src/main/java/com/konecta/ecom_mircoservices/EcomMircoservicesApplication.java
@@ -0,0 +1,13 @@
+package com.konecta.ecom_mircoservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomMircoservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomMircoservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/konecta/ecom_mircoservices/EcomMircoservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konecta.ecom_mircoservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomMircoservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
