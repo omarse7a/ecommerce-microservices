edbe5b7968059343f63b542070109aff52234d61
omarse7a <omarsamehcs@gmail.com>
2025-09-15 01:31:59 +0300

    feat(cart): initialize the cart service and configure redis datastore

---
+++ dotnet-services/CartService/Models/CartItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CartService.Models
+{
+    public class CartItem
+    {
+        public long Id { get; set; }
+        public int quantity { get; set; }
+        public DateTime expiryTime { get; set; } = DateTime.Now.AddHours(24);
+        public long userId { get; set; }
+        public long productId { get; set; }
+    }
+}
